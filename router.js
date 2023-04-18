e15938f7d8f3180bfd27fedd61ef5efc12f823bf
Toscan <dipointg@gmail.com>
2023-04-18 16:06:19 +0200

    fixing the routes

--- router.js
+++ router.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+// const appointmentRouter = require("./routes/appointment");
 
 /* home page */
 router.use("/", indexRouter);
 
-router.use("/users", usersRouter);
+/* authentication */
+router.use("/auth", authRouter);
+// /* users */
+router.use("/users", userRouter)
+// /* appointment */
+// router.use("appointment", appointmentRouter)
+
 
 module.exports = router;
