07bd8d058611f5bf3b5582be6ecd1b805f16304a
Toscan <dipointg@gmail.com>
2023-04-21 17:33:33 +0200

    creation of User Register

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 const userController = require("../controllers/userController");
-const verifyToken = require("../middlewares/verifyToken");
-const isDoctor = require("../middlewares/isDoctor");
-const isAdmin = require("../middlewares/isAdmin");
-const isPatient = require("../middlewares/isPatient");
+const verifyToken = require("../middleware/verifyToken");
+const isDoctor = require("../middleware/isDoctor");
+const isAdmin = require("../middleware/isAdmin");
+const isPatient = require("../middleware/isPatient");
 
 router.get("/profile", verifyToken, userController.getProfile);
 router.put("/profile/update", verifyToken, userController.updateProfile);
@@ -20,12 +20,12 @@ router.get(
   isAdmin,
   userController.getAllPatients
 );
-router.get(
-    "/profile/checkalldoctors",
-    verifyToken,
-    isAdmin,
-    userController.getAllDoctors
-  );
+// router.get(
+//     "/profile/checkalldoctors",
+//     verifyToken,
+//     isAdmin,
+//     userController.getAllDoctors
+//   );
 // router.get(
 //   "/appointments/checkall",
 //   verifyToken,
