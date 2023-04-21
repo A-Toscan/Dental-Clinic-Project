57ce3e5c7d81a0940eedf81c9f5c21c356feaee4
Toscan <dipointg@gmail.com>
2023-04-21 13:59:31 +0200

    fixing problems with the routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const isAdmin = require("../middlewares/isAdmin")
 
-router.post("/register", authController.register);
+router.post("/register/", authController.register);
 router.post("/login", authController.login);
+// router.post("/register/dentist", verifyToken, isAdmin ,authController.registerOdontologo);
 
 module.exports = router;
